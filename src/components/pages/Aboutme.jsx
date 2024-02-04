5cfc1249d71a9a0406d1fbed3c848dc2ef99dfe1
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-04 13:47:09 -0600

    added css styling and a different headshot to about me

--- src/components/pages/Aboutme.jsx
+++ src/components/pages/Aboutme.jsx
@@ -1,14 +1,21 @@
-import '../../styles/aboutme.css';
+import "../../styles/aboutme.css";
 
 export default function Aboutme() {
-    return (
-      <div>
-        <h1>Chad Edwards</h1>
-     <div className='headShot'></div>
-        <p>
-      
-        </p>
+  return (
+    <div className="aboutMeContainer1">
+      <div className='headShotcontainer'>
+      <div className="headShot"></div>
       </div>
-    );
-  }
-  
+      <div className="headerName">
+        <p> CHAD EDWARDS</p>
+      </div>
+      <div className="aboutMeQuote">
+
+      </div>
+      <div className="aboutmeSection">
+      Hey there! I'm a Junior Software Developer based in Dallas, infusing a mix of tech prowess and a genuine love for exploring new places. Picture this: weekdays spent coding, weekends exploring the city's hidden gems. I'm all about finding that sweet spot between lines of code and discovering what makes each place unique. So, let's chat about coding, world-traveling, or anything in between—excited to share the journey!
+      </div>
+    </div>
+
+  );
+}

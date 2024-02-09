3e6a4c599fcb3f3cc89f453e99b5892cd20dff8d
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-09 14:26:48 -0600

    added footer with links to socials

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,1 +1,35 @@
-// Small row of links to where you can find me and such 
+import React from "react";
+import '../styles/footer.css';
+
+import linkedin from "./pages/altIcons/linkedin.png"
+import github from "./pages/altIcons/Github.png"
+
+
+export default function footer() {
+
+  return (
+<div className="footerSection">
+      <div className="links">
+      <a href="https://github.com/chadedwardsofficial"
+            target="_blank" className="link">
+            <img src={github}></img></a> 
+            <a href="https://github.com/chadedwardsofficial"
+            target="_blank" className="link"> <p>GitHub</p> </a>
+            </div>
+            <div className="links">
+            <a href="https://www.linkedin.com/in/chadedwardsofficial/"
+            target="_blank" className="link">
+            <img src={linkedin}></img></a> 
+            <a href="https://www.linkedin.com/in/chadedwardsofficial/"
+            target="_blank" className="link"><p>LinkedIn</p> </a>
+            </div>
+   
+
+</div>
+
+
+  )
+
+
+
+}
--- src/components/portfolioContainer.jsx
+++ src/components/portfolioContainer.jsx
@@ -1,37 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
 import NavTabs from "./NavTabs";
+import Footer from "./Footer";
 import Aboutme from "./pages/Aboutme";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/Resume";
 
-export default function PortfolioContainer(){
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Aboutme");
 
-    const [currentPage, setCurrentPage] = useState('Aboutme');
+  const renderPage = () => {
+    if (currentPage === "Aboutme") {
+      return <Aboutme />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
 
-    const renderPage = () => {
-        if (currentPage === 'Aboutme') {
-          return <Aboutme />;
-        }
-        if (currentPage === 'Portfolio') {
-          return <Portfolio />;
-        }
-        if (currentPage === 'Contact') {
-          return <Contact />;
-        }
-        return <Resume />;
-      };
+  const handlePageChange = (page) => setCurrentPage(page);
 
-      const handlePageChange = (page) => setCurrentPage(page);
-
-
-
-      return (
-        <div>
-     
-          <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-          <main className="mx-3">{renderPage()}</main>
-        </div>
-      );
-
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main className="mx-3">{renderPage()}</main>
+      <Footer />
+    </div>
+  );
 }
